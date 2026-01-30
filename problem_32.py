252497bce06eab67c39ec14922c1af6cc7809c2d
lokeshm25ec-creator <lokesh.m_25ec@kgkite.ac.in>
2026-01-30 15:01:19 +0530

    coded

--- problem_32.py
+++ problem_32.py
@@ -3,7 +3,11 @@
 
 numbers = [45, 12, 78, 34, 89]
 minimum = numbers[0]
-for num in numbers:
+
+for num in numbers[1:]:
+    
     if num < minimum:
         minimum = num
+
 print(f"Minimum: {minimum}")
+
